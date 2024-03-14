dccb2b21ada3eaf44843b6cb1f4601ab676eb98d
NicoRomeroEckerdt <163353586+NicoRomeroEckerdt@users.noreply.github.com>
2024-03-14 11:12:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOPractica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Telefono t1 = new Telefono("A32", "Samsung");
+            Console.WriteLine("Ingrese su número de télefono:");
+            t1.NumeroTelefonico = Console.ReadLine();
+
+            Console.WriteLine("Ingrese su código de operador:");
+            t1.CodigoOperador = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(t1.Llamar());
+            Console.WriteLine(t1.Llamar("Nico"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Telefono.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOPractica
+{
+    internal class Telefono
+    {
+        public string Modelo { get; }
+        public string Marca { get; }
+        public string NumeroTelefonico { get; set; }
+
+        private int codigoOperador;
+
+        public int CodigoOperador
+        {
+            get { return codigoOperador; }
+            set
+            {
+                if (value < 4 && value > 0)
+                {
+                    codigoOperador = value;
+                }
+                else
+                {
+                    codigoOperador = 0;
+                }
+            }
+        }
+        // CONSTRUCTOR
+
+        public Telefono (string marca, string modelo)
+        {
+            Marca = marca;
+            Modelo = modelo;
+        }
+        //metodos
+        public string Llamar()
+        {
+            return "Realizando llamada";
+        }
+        public string Llamar(string contacto)
+        {
+            return "Llamando a " + contacto;
+        }
+
+    }
+}
